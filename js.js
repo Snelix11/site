9a66a2da41e81c68eb3a655722d3bba700593591
carlos <zsnelix@gmail.com>
2022-07-01 18:34:26 -0300

    small update

--- js.js
+++ js.js
@@ -1,9 +1,11 @@
 const btn = document.getElementById('btn')
 
-function toggleMenu(){
+function toggleMenu(event){
+    if (event.type === 'touchstart') event.preventDefault()
     const nav = document.getElementById('menu')
     nav.classList.toggle('active')
 
 }
 
 btn.addEventListener('click', toggleMenu)
+btn.addEventListener('touchstart', toggleMenu)
